a3837ac3b3d9846bc11a01aa68e9ae6b7ae251e9
Saurav Chakraborty <38391547+sauravchakraborty13069@users.noreply.github.com>
2020-10-14 14:30:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response
+#from flask import Flask, request, Response
 from botbuilder.core import BotFrameworkAdapter, BotFrameworkAdapterSettings, ConversationState,MemoryStorage
 from botbuilder.schema import Activity
 import asyncio
