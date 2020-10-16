3875b24c41c4d6bcaa34f322b45cfe1e3bdc157d
swwapnily4io <swwapnilyadav@gmail.com>
2020-10-16 23:27:46 +0530

    accounts and transaction history changes

--- adaptive_cards/transactionhistory_adaptive_card.py
+++ adaptive_cards/transactionhistory_adaptive_card.py
@@ -4,7 +4,7 @@ def transaction_history_card(data):
     transactionHistoryCard = AdaptiveCard(
         backgroundImage="https://image.freepik.com/free-photo/wall-wallpaper-concrete-colored-painted-textured-concept_53876-31799.jpg")
     transactionHistoryCard.add(
-        TextBlock(text="Savings Account xxxxxxxxx4567 - Balance is  $"+str(data["account"]["balance"]), color="light", size="Medium",
+        TextBlock(text="Savings Account xxxxxxxxx4567 - Balance is  "+str(data["currencySymbol"])+str(data["account"]["balance"]), color="light", size="Medium",
                   weight="Bolder"))
     transactionHistoryCard.add(
         TextBlock(text="Last 10 transaction details:", color="light", size="Medium", weight="Bolder"))
@@ -12,31 +12,31 @@ def transaction_history_card(data):
     if (data["statusMsg"] is not None and data["statusMsg"] in ("Success")):
         print("success response ", data)
         historyList = data["historyList"]
-        transactionHistoryCard.add(Column(width=100))
+        transactionHistoryCard.add(Column(width=1))
         transactionHistoryCard.add(TextBlock(text="TRANSACTION DATE", color="light", weight="Bolder"))
         for lst in historyList:
             print(lst['transactionDate'])
             transactionHistoryCard.add(TextBlock(text=lst['transactionDate'], color="light"))
         transactionHistoryCard.up_one_level()
-        transactionHistoryCard.add(Column(width=100))
+        transactionHistoryCard.add(Column(width=1))
         transactionHistoryCard.add(TextBlock(text="REF. NO", color="light", weight="Bolder"))
         for lst in historyList:
             print(lst['refNo'])
             transactionHistoryCard.add(TextBlock(text=str(lst['refNo']), color="light"))
         transactionHistoryCard.up_one_level()
-        transactionHistoryCard.add(Column(width=100))
+        transactionHistoryCard.add(Column(width=1))
         transactionHistoryCard.add(TextBlock(text="TRANSACTION TYPE", color="light", weight="Bolder"))
         for lst in historyList:
             print(lst['transactionType'])
             transactionHistoryCard.add(TextBlock(text=lst['transactionType'], color="light"))
         transactionHistoryCard.up_one_level()
-        transactionHistoryCard.add(Column(width=100))
+        transactionHistoryCard.add(Column(width=1))
         transactionHistoryCard.add(TextBlock(text="AMOUNT", color="light", weight="Bolder"))
         for lst in historyList:
             print(lst['amount'])
             transactionHistoryCard.add(TextBlock(text=str(lst['amount']), color="light"))
         transactionHistoryCard.up_one_level()
-        transactionHistoryCard.add(Column(width=100))
+        transactionHistoryCard.add(Column(width=1))
         transactionHistoryCard.add(TextBlock(text="DESCRIPTION", color="light", weight="Bolder"))
         for lst in historyList:
             print(lst['description'])
--- cards/send_accountbalance_card.py
+++ cards/send_accountbalance_card.py
@@ -7,6 +7,37 @@ from botbuilder.schema import (
     ActionTypes,
 )
 
+from adaptivecardbuilder import Column, ColumnSet, Image, TextBlock, AdaptiveCard, InputText, ActionShowCard, ActionSubmit, ActionOpenUrl
+
+
+def buildAccountBalance_card(data):
+    accountsCard = AdaptiveCard(
+        backgroundImage=
+        "https://image.freepik.com/free-photo/wall-wallpaper-concrete-colored-painted-textured-concept_53876-31799.jpg"
+    )
+    accountsCard.add(
+        TextBlock(
+            text=
+            "Account Details \n\n Please select an account to see last 10 transaction details",
+            color="light",
+            size="Medium",
+            weight="Bolder"))
+    if (data["statusMsg"] is not None and data["statusMsg"] in ("Success")):
+        print("success response ", data)
+        accountsList = data["accountList"]
+
+        for lst in accountsList:
+            print(lst['accountType'])
+            accountsCard.add(
+                ActionSubmit(title="" + str(lst['accountType']) +
+                           "\n Account No. " + str(lst['accountNo']) +
+                           "\n Opened date: " + str(lst['createdOn']) +
+                           "\n Balance is:" + str(lst["currencySymbol"]) +
+                           str(lst["balance"]),
+                           value=str(lst['accountNo'])))
+    return accountsCard
+
+
 def accountbalance_card():
     card = ThumbnailCard(
         title="Account Details",
@@ -16,14 +47,16 @@ def accountbalance_card():
         buttons=[
             CardAction(
                 type=ActionTypes.im_back,
-                title="Savings Account xxxxxxxxx4567 \n Opened date: 05/10/2010 \n Balance is $112.02",
+                title=
+                "Savings Account No. xxxxxxxxx4567 \n Opened date: 05/10/2010 \n Balance is $112.02",
                 text="Balance is $112.02",
                 display_text="Balance is $112.02",
                 value="xxxxxxxxx4567",
             ),
             CardAction(
                 type=ActionTypes.im_back,
-                title="Current Account xxxxxxxxx4566 \n Opened date: 05/10/2012 \n Balance is $0.00",
+                title=
+                "Current Account xxxxxxxxx4566 \n Opened date: 05/10/2012 \n Balance is $0.00",
                 text="Balance is $0.00",
                 display_text="Balance is $0.00",
                 value="xxxxxxxxx4566",
